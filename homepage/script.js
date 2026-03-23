f92a3170de056c91fd23bf051d0099828261c797
EDALIMEDAULo1 <demiladeifedayo@gmail.com>
2026-03-23 15:15:18 +0100

    made changes

--- homepage/script.js
+++ homepage/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   const navSvg = document.getElementById('nav-svg');
-  const navLinks = document.getElementsByClassName('nav-links');
+  const navLinks = document.querySelector('.nav-links');
 
   navSvg.addEventListener('click', function(){
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('nav-links');
   });
 });
